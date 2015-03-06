2897e44678cb18992f55669fd74b491fd036d056
NicoleWiller <n._w@hotmail.de>
2015-03-06 16:47:33 +0100

    Create movement.h

---
+++ movement.h
@@ -0,0 +1,15 @@
+*movement.h
+
+#ifndef H_MOVEMENT_H_
+#define H_MOVEMENT_H_
+
+void rotate();
+void forward();
+void stop();
+void search();
+void back();
+void correction();
+void search_line_right();
+void search_line_left();
+
+#endif /* H_MOVEMENT_H_ */
